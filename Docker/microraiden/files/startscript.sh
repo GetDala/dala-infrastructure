c74d5db7f15cab4d8548e16d75f014167601001d
Jared Leonard <jaredmyronarthurleonard@gmail.com>
2018-03-26 16:04:45 +0200

    updated path of key

--- Docker/microraiden/files/startscript.sh
+++ Docker/microraiden/files/startscript.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 [ -e tmp/password ] && rm tmp/password
-PASSWORD=$(aws ssm get-parameter --name '/dala-wallet-api/ropsten/HOT_WALLET_PASSWORD' --with-decryption  --region $AWS_DEFAULT_REGION --query "Parameter.Value" --output text)
+PASSWORD=$(aws ssm get-parameter --name '/dala-wallet-api/ropsten/MICRORAIDEN_RECEIVER_PASSWORD' --with-decryption  --region $AWS_DEFAULT_REGION --query "Parameter.Value" --output text)
 echo "$PASSWORD" > /tmp/password
 chmod 400 /tmp/password
 cd /dala-wallet && python3 -m src.microraiden.dala_proxy --private-key /tmp/private_key.json  --private-key-password-file /tmp/password --rpc-provider $RPC_PROVIDER --state-file "./dala_raiden_proxy.db" --channel-manager-address $CHANNEL_MANAGER_ADDRESS start
