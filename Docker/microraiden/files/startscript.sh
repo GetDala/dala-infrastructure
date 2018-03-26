9e4e05e8f7e374836b66504616469cbbe59932a1
Jared Leonard <jaredmyronarthurleonard@gmail.com>
2018-03-26 21:03:34 +0200

    added chmod on private key

--- Docker/microraiden/files/startscript.sh
+++ Docker/microraiden/files/startscript.sh
@@ -3,4 +3,5 @@
 PASSWORD=$(aws ssm get-parameter --name '/dala-wallet-api/ropsten/MICRORAIDEN_RECEIVER_PASSWORD' --with-decryption  --region $AWS_DEFAULT_REGION --query "Parameter.Value" --output text)
 echo "$PASSWORD" > /tmp/password
 chmod 400 /tmp/password
+chmod 400 /tmp/private_key.json
 cd /dala-wallet && python3 -m src.microraiden.dala_proxy --private-key /tmp/private_key.json  --private-key-password-file /tmp/password --rpc-provider $RPC_PROVIDER --state-file "./dala_raiden_proxy.db" --channel-manager-address $CHANNEL_MANAGER_ADDRESS start
